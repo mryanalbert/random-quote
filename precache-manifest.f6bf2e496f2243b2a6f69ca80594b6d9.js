b965875efcf960fbd14082a6fe115512e5a746d0
Ryan <mryanalbert@gmail.com>
2019-08-13 12:54:05 -0700

    Updates

---
+++ precache-manifest.f6bf2e496f2243b2a6f69ca80594b6d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e32544a13850f9ff1bd63fdd22bfe06",
+    "url": "/random-quote/index.html"
+  },
+  {
+    "revision": "0eae674dc98efb72d67a",
+    "url": "/random-quote/static/css/main.b722ac7b.chunk.css"
+  },
+  {
+    "revision": "2bf73a35bec88ae6e09b",
+    "url": "/random-quote/static/js/2.2aefb932.chunk.js"
+  },
+  {
+    "revision": "0eae674dc98efb72d67a",
+    "url": "/random-quote/static/js/main.0986ddfb.chunk.js"
+  },
+  {
+    "revision": "dab3e2eb83e78457b67d",
+    "url": "/random-quote/static/js/runtime~main.3f09ba65.js"
+  }
+]);
